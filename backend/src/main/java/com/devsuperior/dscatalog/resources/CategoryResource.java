b4418d5bd3592a9353f028d44d929b92ae5a9a74
cunharodrigues <pwcunha@gmail.com>
2021-10-13 13:40:26 -0300

    Find ctegory by id, exception handling

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -1,16 +1,17 @@
 package com.devsuperior.dscatalog.resources;
 
-import java.util.List;
-
+//import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.services.CategoryService;
+//import com.sun.tools.javac.util.List;
 
 @RestController
 @RequestMapping(value = "/categories")
@@ -18,10 +19,17 @@ public class CategoryResource {
 
 	@Autowired
 	private CategoryService service;
-
-	@GetMapping
-	public ResponseEntity<List<CategoryDTO>> findAll() {
-		List<CategoryDTO> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+	
+	//@GetMapping
+	//public ResponseEntity<List<CategoryDTO>> findAll() {
+		//List<CategoryDTO> list = service.findAll();
+		//return ResponseEntity.ok().body(list);
+	//}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<CategoryDTO> findById(@PathVariable Long id) {
+		CategoryDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
+		
 	}
 }
